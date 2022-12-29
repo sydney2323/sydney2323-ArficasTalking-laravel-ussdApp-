d8f16c0c1583917029c772b1598e4536100a67a0
sydney2323 <sydneykb38@gmail.com>
2022-12-29 21:35:46 +0300

    first commit

---
+++ app/Http/Traits/SmsTrait.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Traits;
+  
+use Illuminate\Http\Request;
+
+use AfricasTalking\SDK\AfricasTalking;
+
+trait SmsTrait 
+{
+    public function sendText($message, $phone)
+    {
+        $username = config("africastalking.username_sandbox");
+        $apiKey = config("africastalking.api_key_sandbox");
+
+        $AT = new AfricasTalking($username, $apiKey);
+        
+        $sms = $AT->sms();
+    
+        try {
+            $result = $sms->send([
+                'to'      => $phone,
+                'message' => $message
+            ]);
+                
+            print_r($result);
+        } catch (Exception $e) {
+            echo "Error: ".$e.getMessage();
+        }
+
+        return "I am done";
+    }
+}
